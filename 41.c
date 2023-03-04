417e3486485e7cdca1be5b1f847d7e1d64ed9a99
Mayank Arya <mmaismma@hotmail.com>
2023-03-04 19:11:45 +0530

    First line exception is now only applied to the 2nd alphabet loop Added `return 0`

--- 41.c
+++ 41.c
@@ -8,24 +8,21 @@ int main()
 
     for (int i = size; i; i--)
     {
-        if (i == size)
-        {
-            for (int j = 0; j < i; j++)
-                printf("%c ", 'A' + j);
-            for (int j = i - 2; j >= 0; j--)
-                printf("%c ", 'A' + j);
-            
-            printf("\n");
-            continue;
-        }
-
         for (int j = 0; j < i; j++)
             printf("%c ", 'A' + j);
+
         for (int j = 0; j < size * 4 - 2 - i * 4; j++)
-            printf("%c", ' ');
-        for (int j = i - 1; j >= 0; j--)
-            printf("%c ", 'A' + j);
+            printf(" ");
+
+        if (i == size)
+            for (int j = i - 2; j >= 0; j--)
+                printf("%c ", 'A' + j);
+        else
+            for (int j = i - 1; j >= 0; j--)
+                printf("%c ", 'A' + j);
 
         printf("\n");
     }
+
+    return 0;
 }
