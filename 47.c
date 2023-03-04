659471057ce2de95a670203247b1dd4c0565192c
dev-shivamk <118415844+dev-shivamk@users.noreply.github.com>
2023-03-04 21:25:16 +0530

    Week name

--- 47.c
+++ 47.c
@@ -10,13 +10,13 @@ int main()
     {
         case 1 : printf("Monday"); break;
         case 2 : printf("Tuesday"); break;
-        case 3 : printf("ednesday"); break;
+        case 3 : printf("Wednesday"); break;
         case 4 : printf("Thursday"); break;
         case 5 : printf("Friday"); break;
         case 6 : printf("Saturday"); break;
         case 7 : printf("Sunday"); break;
         default : printf("Please enter number between 1 to 7 ! ");
     }
-
+    printf("\n")
     return 0;
 }
